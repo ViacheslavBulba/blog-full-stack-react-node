3a3e7e175cc170635856a84d43a92a39612328aa
Slava Bulba <megadevices@gmail.com>
2023-01-24 15:26:31 -0800

    show article based on url

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,6 +1,17 @@
+import { useParams } from 'react-router-dom';
+import articles from './article-content';
+
 const Article = () => {
+    const { articleId } = useParams();
+    const article = articles.find(article => article.name === articleId) || {title: 'not found', content: ['not found']};
     return (
-        <h1>Article page!</h1>
+        <>
+        <h1>{article.title}</h1>
+        {article.content.map(paragraph => (
+            <p>{paragraph}</p>
+        ))}
+        </>
+        
     )
 }
 
