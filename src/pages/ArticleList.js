b598a3eed6be384659e87448f4f6d8839ed63e40
Slava Bulba <megadevices@gmail.com>
2023-01-28 14:52:35 -0800

    add links on list page

--- src/pages/ArticleList.js
+++ src/pages/ArticleList.js
@@ -1,14 +1,15 @@
 import articles from './article-content';
+import { Link } from 'react-router-dom';
 
 const ArticleList = () => {
     return (
         <>
             <h1>Articles</h1>
             {articles.map(article => (
-                <div>
+                <Link to={`/articles/${article.name}`} className='article-list-item' key={article.name}>
                     <h3>{article.title}</h3>
                     <p>{article.content[0].substring(0, 150)}...</p>
-                </div>
+                </Link>
             ))}
         </>
     )
