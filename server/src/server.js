72dda45b69ada5dab20339418a5dd331b65b7898
Slava Bulba <megadevices@gmail.com>
2023-01-29 17:54:14 -0800

    add upvote server functionality wirh fake data

--- server/src/server.js
+++ server/src/server.js
@@ -2,13 +2,31 @@ import express from 'express';
 
 const PORT = process.env.PORT || 3001;
 
+let articlesInfo = [{
+    name: 'learn-react',
+    upvotes: 0,
+}, {
+    name: 'learn-node',
+    upvotes: 0,
+}, {
+    name: 'mongodb',
+    upvotes: 0,
+}]
+
 const app = express();
 app.use(express.json());
 
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
-  });
+app.put('/api/articles/:name/upvote', (req, res) => {
+    const { name } = req.params;
+    const article = articlesInfo.find(a => a.name === name);
+    if (article) {
+        article.upvotes += 1;
+        res.send(`The ${name} article now has ${article.upvotes} upvotes`);
+    } else {
+        res.send('That article doesn\'t exist');
+    }
+});
 
 app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+    console.log(`Server listening on ${PORT}`);
 });
