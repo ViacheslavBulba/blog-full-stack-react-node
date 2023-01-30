4cc0f98c5abcfa577f1d25ae01b60ccab16bdb93
Slava Bulba <megadevices@gmail.com>
2023-01-29 22:45:29 -0800

    add post('/api/articles/:name/comments'

--- server/src/server.js
+++ server/src/server.js
@@ -5,12 +5,15 @@ const PORT = process.env.PORT || 3001;
 let articlesInfo = [{
     name: 'learn-react',
     upvotes: 0,
+    comments: [],
 }, {
     name: 'learn-node',
     upvotes: 0,
+    comments: [],
 }, {
     name: 'mongodb',
     upvotes: 0,
+    comments: [],
 }]
 
 const app = express();
@@ -24,6 +27,18 @@ app.put('/api/articles/:name/upvote', (req, res) => {
         res.send(`The ${name} article now has ${article.upvotes} upvotes`);
     } else {
         res.send('That article doesn\'t exist');
+    }
+});
+
+app.post('/api/articles/:name/comments', (req, res) => {
+    const { name } = req.params;
+    const { postedBy, text } = req.body;
+    const article = articlesInfo.find(a => a.name === name);
+    if (article) {
+        article.comments.push({ postedBy, text });
+        res.send(article.comments);
+    } else {
+        res.send('That article doesn\'t exist!');
     }
 });
 
