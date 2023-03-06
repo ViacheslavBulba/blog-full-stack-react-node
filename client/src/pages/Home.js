d12b263579415936e36f798fded0e9a3e9c03573
Slava Bulba <megadevices@gmail.com>
2023-03-05 17:51:52 -0800

    add content to articles

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,18 @@
 const Home = () => {
     return (
-        <h1>Home page!</h1>
+        <>
+            <h1>Welcome to the blog!</h1>
+            <div>
+                You've learned React and can create amazing front-end interfaces. 
+                But deploying them requires more. 
+                You need logic — and a place to put it. 
+                By combining your front-end UI with a back-end solution and cloud hosting, 
+                you can build powerful and interactive full-stack applications. 
+                In this project we learn how to combine React and Node.js 
+                in a full-stack, full-featured website. 
+                Learn how to create an interface from React components and develop a Node.js server.
+            </div>
+        </>
     )
 }
 
--- server/src/article-content.js
+++ server/src/article-content.js
@@ -3,75 +3,34 @@ const articles = [
         name: 'learn-react',
         title: 'The Fastest Way to Learn React',
         content: [
-            `Welcome! Today we're going to be talking about the fastest way to
-            learn React. We'll be discussing some topics such as proin congue
-            ligula id risus posuere, vel eleifend ex egestas. Sed in turpis leo. 
-            Aliquam malesuada in massa tincidunt egestas. Nam consectetur varius turpis, 
-            non porta arcu porttitor non. In tincidunt vulputate nulla quis egestas. Ut 
-            eleifend ut ipsum non fringilla. Praesent imperdiet nulla nec est luctus, at 
-            sodales purus euismod.`,
-            `Donec vel mauris lectus. Etiam nec lectus urna. Sed sodales ultrices dapibus. 
-            Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan 
-            nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus 
-            sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor 
-            interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id 
-            consequat quam. Vivamus accumsan dui in facilisis aliquet.`,
-            `Etiam nec lectus urna. Sed sodales ultrices dapibus. 
-            Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan 
-            nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus 
-            sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor 
-            interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id 
-            consequat quam. Vivamus accumsan dui in facilisis aliquet.`,
+            "React is a JavaScript library for building user interfaces and front-end applications.",
+            "If you’re interested in building User Interfaces (UIs), you’ll likely want to learn React. However, figuring out where to start can be challenging. There are multiple levels ranging from beginner to advanced. What’s more, there are a handful of elements that make it different from other libraries and frameworks.",
+            "The good news is that there are plenty of resources out there that can help simplify the learning process. With the right knowledge, tools, and guidance, you can gain a sound understanding of React. That way, you can start using it in your development projects.",
+            "In this post, we’ll explain what React is and discuss the benefits of learning it. Then, we’ll go over some skills and topics to review before you can explore React. Finally, we’ll provide you with information and resources that you can use to learn React as fast (and affordably) as possible. Let’s jump in!",
+            "Before learning React, there are a few things developers should have a working knowledge of first. Let’s take a look at some of the most important ones.",
+            "JavaScript: React is a JavaScript library, so it’s important that you learn JavaScript have a good understanding of the language before you start.",
+            "HTML and CSS: Learning HTML and CSS is strongly recommended for learning React since its applications are typically written in HTML and CSS.",
+            "Git: React applications are often stored in a Git repository, so it’s important to understand how to use this version control system.",
+            "Webpack: As we mentioned, Webpack is a tool that can be used to bundle your React application into a single file for deployment.",
+            "Babel: Babel is a tool that can be used to transpile your React code into JavaScript that can be run in the browser.",
+            "React Router: This is a library that you can use to manage the routing in your React application.",
+            "Redux: This library can help you manage state in your React application."
         ]
-    },    {
+    }, {
         name: 'learn-node',
         title: 'How to Build a Node Server in 10 Minutes',
         content: [
-            `In this article, we're going to be talking looking at a very quick way
-            to set up a Node.js server. We'll be discussing some topics such as proin congue
-            ligula id risus posuere, vel eleifend ex egestas. Sed in turpis leo. 
-            Aliquam malesuada in massa tincidunt egestas. Nam consectetur varius turpis, 
-            non porta arcu porttitor non. In tincidunt vulputate nulla quis egestas. Ut 
-            eleifend ut ipsum non fringilla. Praesent imperdiet nulla nec est luctus, at 
-            sodales purus euismod.`,
-            `Donec vel mauris lectus. Etiam nec lectus urna. Sed sodales ultrices dapibus. 
-            Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan 
-            nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus 
-            sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor 
-            interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id 
-            consequat quam. Vivamus accumsan dui in facilisis aliquet.`,
-            `Etiam nec lectus urna. Sed sodales ultrices dapibus. 
-            Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan 
-            nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus 
-            sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor 
-            interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id 
-            consequat quam. Vivamus accumsan dui in facilisis aliquet.`,
+            `In this article, we are going to take a look at a very quick way to set up a Node.js server.`,
+            `Coming soon...`,
         ]
-    },     {
+    }, {
         name: 'mongodb',
         title: 'Learn MongoDB',
         content: [
-            `Today is the day I talk about something which scares most people: resumes.
-            In reality, I'm not sure why people have such a hard time with proin congue
-            ligula id risus posuere, vel eleifend ex egestas. Sed in turpis leo. 
-            Aliquam malesuada in massa tincidunt egestas. Nam consectetur varius turpis, 
-            non porta arcu porttitor non. In tincidunt vulputate nulla quis egestas. Ut 
-            eleifend ut ipsum non fringilla. Praesent imperdiet nulla nec est luctus, at 
-            sodales purus euismod.`,
-            `Donec vel mauris lectus. Etiam nec lectus urna. Sed sodales ultrices dapibus. 
-            Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan 
-            nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus 
-            sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor 
-            interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id 
-            consequat quam. Vivamus accumsan dui in facilisis aliquet.`,
-            `Etiam nec lectus urna. Sed sodales ultrices dapibus. 
-            Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan 
-            nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus 
-            sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor 
-            interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id 
-            consequat quam. Vivamus accumsan dui in facilisis aliquet.`,
+            `In this article, we are going to take a look at a very quick way to set up MongoDB.`,
+            `Coming soon...`,
         ]
-    },  
+    },
 ];
 
 export default articles;
